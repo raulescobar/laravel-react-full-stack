ef3087a3e7f880d0894a3cb537daf42fbcd37e41
Raúl Escobar <raulescobarlopez@gmail.com>
2023-07-07 18:26:24 -0500

    Creación de rutas protegidas

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useStateContext } from '../contexts/ContextProvider';
 
 Outlet
 
 export default function DefaultLayout() {
+
+  const {user,token} = useStateContext()
+
+  if(!token){
+    return <Navigate to="/login" />
+  }
+
   return (
     <div>
+      Default
       <Outlet />
     </div>
   )
